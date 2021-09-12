9a2a9a14fc5fb22f7a071edcbbfb12e7fba6d85a
codercast <75508949+codercast@users.noreply.github.com>
2021-09-12 08:11:08 -0700

    Step 2 Complete

--- src.php
+++ src.php
@@ -4,7 +4,9 @@
 $num;
 for ($num = 1; $num <= 20; $num++)
  {
-    if ($num % 15 == 0)
+    if (str_contains($num, 3) == true) 
+        echo "lucky ";
+    else if ($num % 15 == 0)
         echo "fizzbuzz ";
     else if (($num % 3) == 0)
         echo "fizz ";
